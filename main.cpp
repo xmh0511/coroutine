4f24b1d47e27744281686f8a9f2f0c22e5d2dea5
xmh0511 <970252187@qq.com>
2019-12-10 10:38:47 +0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ int main(){
 	ctx.regs[RegIndex::Ret] = (void*)&CoroutinFunc;
 	char* sp = ctx.stack + ctx.stack_size;
 	ctx.regs[RegIndex::Rbp] = 0;
-	ctx.regs[RegIndex::Rsp] = (char*)((unsigned long)sp & -16LL) - 8;//保奇偶性  pushq  ebp  地址为8的偶数倍
+	ctx.regs[RegIndex::Rsp] = (char*)((unsigned long)sp & -16LL) - 8;//保奇偶性  pushq  ebp  地址为平台寄存器位数的偶数倍
 	ctx.regs[RegIndex::Rdi] = (void*)&ctx;
 	ctx.regs[RegIndex::Rsi] = (void*)0;
 	memset(&current,0,sizeof(context));
